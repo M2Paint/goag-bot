43fd982a66bfdaab048f60f7b7b55f7b670d31ec
Unknown <tonyv2576@gmail.com>
2017-06-04 12:18:05 -0400

    I swear if he crashes I'll stab him.

--- index.js
+++ index.js
@@ -65,7 +65,6 @@ bot.on('message', function(message) {
         case "chat":
             if(args[1].length) {
                 message.channel.send({embed: {
-                    color: FFFFFF,
                     title: message.author.name,
                     description: message.content.substring(prefix.length + 4)
                 }});
